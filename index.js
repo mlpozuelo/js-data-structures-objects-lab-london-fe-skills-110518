cf640e5c9e627da6aec9338bef4c4aad0a37a33a
Maria Lopez Pozuelo <mlpozuelo@google.com>
2019-01-17 09:28:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+//First function updateDriver//
+
+//Define Driver Object/
+const driver = {
+  driverName: 'John',
+  driverCar: 'Ford'
+}
+
+//updateDriverWithKeyAndValue//
+
+function updateDriverWithKeyAndValue (driver,key,value) {
+  const newObj = {...driver};
+  
+  newObj[key] = value;
+  
+  return newObj
+}
+
+//destructivelyUpdateDriverWithKeyAndValue//
+
+function destructivelyUpdateDriverWithKeyAndValue (driver,key,value){
+
+driver [key] = value;
+ 
+return driver
+}
+
+
+
